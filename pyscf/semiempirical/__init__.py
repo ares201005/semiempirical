175adce1cd8762eec8ba6ad807b9fae1802ffb9c
ares201005 <ares201005@gmail.com>
2023-07-14 10:39:29 -0600

    Chance develop branch: Add  parameters MNDO PM3 Add nddo.py Reorganize parameters Initial implementation of OMX methods

--- pyscf/semiempirical/__init__.py
+++ pyscf/semiempirical/__init__.py
@@ -1,4 +1,6 @@
 from .mindo3 import RMINDO3, UMINDO3
+from .mndo_class import RMNDO
+from .omx_class import ROM2
 
 __version__ = '0.1.0'
 
@@ -7,3 +9,11 @@ def MINDO3(mol):
         return RMINDO3(mol)
     else:
         return UMINDO3(mol)
+
+def MNDO(mol, model):
+    if mol.spin == 0:
+        return RMNDO(mol, model)
+
+def OMX(mol, model):
+    if mol.spin == 0:
+        return ROM2(mol, model)
