2b1173915883935c73d01f7b6dad3b2ec7c37a4f
Chance Lander <chance@n153.cluster>
2023-10-09 10:35:54 -0500

    update res

--- mytest/debug.sh
+++ mytest/debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=express
+#SBATCH --partition=batch
 #SBATCH --ntasks=8
 #SBATCH --tasks-per-node=8
 ##SBATCH --output=%J_stdout.txt
@@ -15,5 +15,8 @@ eval "$(conda shell.bash hook)"
 source ~/mambaforge/etc/profile.d/mamba.sh
 
 mamba activate pybase
+#python test_mindo3.py
+#python test_am1.py
+#python am1_energy.py
 python test_om2.py
 mamba deactivate 
