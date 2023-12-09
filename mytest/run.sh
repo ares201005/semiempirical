224c205a2a43adf019b191f7f7121be4d138955f
Chance Lander <chancelander@Chances-MacBook-Pro.local>
2023-12-09 15:42:04 -0600

    rm swp file

--- mytest/run.sh
+++ mytest/run.sh
@@ -3,6 +3,8 @@
 eval "$(conda shell.bash hook)"
 source /Applications/anaconda3/etc/profile.d/conda.sh
 
+export LIBSEMI=/Users/chancelander/Documents/Shao/semiempirical/code/semiempirical/build/lib.macosx-10.9-x86_64-cpython-38/pyscf/semiempirical/lib/libsemiempirical.so
+
 conda activate pysemi
 python --version
 python3 --version
